38e0efa258b5f80845de77cdf23a8e6b9c1fec5a
Beka Sturua <beqasturua3@gmail.com>
2022-07-16 00:31:09 +0400

    add resize element

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import React, { useState, useEffect } from "react";
 function App() {
-  const [resourceType, setResourceType] = useState("posts");
-  const [items, setItems] = useState([]);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
+  const handleResize = () => {
+    setWindowWidth(window.innerWidth);
+  };
   useEffect(() => {
-    fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
-      .then((response) => response.json())
-      .then((json) => setItems(json));
-  }, [resourceType]);
-  return (
-    <>
-      <div>
-        <button onClick={() => setResourceType("posts")}>Posts</button>
-        <button onClick={() => setResourceType("users")}>users</button>
-        <button onClick={() => setResourceType("coments")}>coments</button>
-      </div>
-      <h1>{resourceType}</h1>
-      {items.map((item) => {
-        return <pre>{JSON.stringify(item)}</pre>;
-      })}
-    </>
-  );
+    window.addEventListener("resize", handleResize);
+  });
+  return <div>{windowWidth}</div>;
 }
 
 export default App;
