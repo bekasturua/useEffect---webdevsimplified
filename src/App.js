36816f2c99b46574f9a53eebf214e1e5f43fccef
Beka Sturua <beqasturua3@gmail.com>
2022-07-16 00:23:53 +0400

    return users

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 function App() {
   const [resourceType, setResourceType] = useState("posts");
+  const [items, setItems] = useState([]);
 
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+      .then((response) => response.json())
+      .then((json) => setItems(json));
+  }, [resourceType]);
   return (
     <>
       <div>
@@ -10,6 +16,9 @@ function App() {
         <button onClick={() => setResourceType("coments")}>coments</button>
       </div>
       <h1>{resourceType}</h1>
+      {items.map((item) => {
+        return <pre>{JSON.stringify(item)}</pre>;
+      })}
     </>
   );
 }
