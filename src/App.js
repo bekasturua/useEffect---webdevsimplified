30eacd4fb796d268a09eb4137bc69f5df55bb3e1
Beka Sturua <beqasturua3@gmail.com>
2022-07-16 00:13:21 +0400

    start project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import logo from "./logo.svg";
-import "./App.css";
-
+import React, { useState } from "react";
 function App() {
-  
+  const [resourceType, setResourceType] = useState("posts");
+
+  return (
+    <>
+      <div>
+        <button onClick={() => setResourceType("posts")}>Posts</button>
+        <button onClick={() => setResourceType("users")}>users</button>
+        <button onClick={() => setResourceType("coments")}>coments</button>
+      </div>
+      <h1>{resourceType}</h1>
+    </>
+  );
 }
 
 export default App;
